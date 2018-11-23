9fd94ac30733f4076b4bd8e9140fde3b56b7ad09
BroderickJack <broderick.jack13@gmail.com>
2018-11-22 21:10:22 -0500

    Added differnce of gaussian

--- main.m
+++ main.m
@@ -4,3 +4,20 @@
 clear; clc; close all;
 
 % This script runs the SIFT algorithm and displays the results
+
+
+%% Define the variables
+imageName = './TestImages/im1.jpg';
+
+%% Load the image
+% Convert to grayscale and then to double
+im = im2double( rgb2gray( imread(imageName) ) );
+
+%% Call the SIFT algorithm to get interest points
+[xPoints, yPoints] = SIFT(im);
+
+%% Show the interest points
+figure(); hold on;
+imshow(im);
+plot(xPoints, yPoints, 'rx');
+legend('Interest Points');
