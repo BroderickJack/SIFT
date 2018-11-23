7f3c2d63db47528069dcc969504fc8ec59a73ac5
BroderickJack <broderick.jack13@gmail.com>
2018-11-22 20:27:30 -0500

    added SIFT function and main script

---
+++ SIFT.m
@@ -0,0 +1,7 @@
+function [xPoints, yPoints] = SIFT(im)
+% INPUTS
+%   im: nxm array of points in a grayscale image
+% OUTPUTS
+%   xPoints: X-coordinates of the interest points
+%   yPoints: Y-coordinates of the interest points
+end
---
+++ main.m
@@ -0,0 +1,6 @@
+% Jack Broderick
+% November 22, 2018
+% CS652 Final Project
+clear; clc; close all;
+
+% This script runs the SIFT algorithm and displays the results
